d61c45621fe8de7ba41a03dd95ecb3eebb2c00e9
namaymahindru <namaym9@gmail.com>
2024-02-25 23:24:47 +0530

    25.2.24 Sunday

---
+++ PartitionSubsetSum.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    bool subsetSumToK(int n, int k, vector<int> &arr)
+    {
+        // Write your code here.
+
+        vector<vector<bool>> dp(n, vector<bool>(k + 1, 0));
+
+        for (int i = 0; i < n; i++)
+        {
+            dp[i][0] = true;
+        }
+
+        dp[0][arr[0]] = true;
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int target = 1; target <= k; target++)
+            {
+                bool notTake = dp[ind - 1][target];
+                bool take = false;
+
+                if (arr[ind] <= target)
+                {
+                    take = dp[ind - 1][target - arr[ind]];
+                }
+
+                dp[ind][target] = take | notTake;
+            }
+        }
+
+        return dp[n - 1][k];
+        bool canPartition(vector<int> & nums)
+        {
+        }
+    };
